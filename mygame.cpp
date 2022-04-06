30a19f59239988ee465bffd3c628b73d59c3bf35
ujjwal-2706 <ujjwalmehta2002@gmail.com>
2022-04-06 14:00:59 +0530

    created a small game with background

--- mygame.cpp
+++ mygame.cpp
@@ -8,6 +8,7 @@ SDL_Renderer* render = NULL;
 SDL_Surface* surface1 = NULL;
 SDL_Rect destination;
 SDL_Texture* text;
+SDL_Texture* text2;
 int main(int argc,char* args[])
 {
     destination.x = 0;
@@ -23,7 +24,13 @@ int main(int argc,char* args[])
     surface1 = SDL_LoadBMP("hello_world.bmp");
     text = SDL_CreateTextureFromSurface(render,surface1);
     SDL_FreeSurface(surface1);
+    surface1 = IMG_Load("code.png");
+    text2 = SDL_CreateTextureFromSurface(render,surface1);
+    SDL_FreeSurface(surface1);
     cout << SDL_GetTicks() << endl;
+    SDL_RenderCopy(render,text2,NULL,NULL);
+    SDL_RenderCopy(render,text,NULL,&destination);
+    SDL_RenderPresent(render);
     while (!quit)
     {
 
@@ -66,9 +73,14 @@ int main(int argc,char* args[])
         destination.x = xpos;
         destination.y = ypos;
         SDL_RenderClear(render);
+        SDL_RenderCopy(render,text2,NULL,NULL);
         SDL_RenderCopy(render,text,NULL,&destination);
         SDL_RenderPresent(render);
     }
+    SDL_DestroyRenderer(render);
+    SDL_DestroyTexture(text);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     cout << "ending game" << endl;
     return 0;
 }
